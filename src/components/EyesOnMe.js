e315628a093f3bb0502a52fe895baa67d5bb8e94
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-04-25 11:58:46 +0300

    Event Handling in React

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+// components/EyesOnMe.js
+import React from 'react';
+
+const EyesOnMe = () => {
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <div>
+      <button onFocus={handleFocus} onBlur={handleBlur}>
+        Eyes on me
+      </button>
+    </div>
+  );
+};
+
+export default EyesOnMe;
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,28 @@
-// Code Keypad Component Here
+// components/Keypad.js
+import React, { useState } from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+const Keypad = ({ onInput }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+    console.log('Entering password...');
+    if (onInput) {
+      onInput(event.target.value);
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor="keypadInput">Enter password:</label>
+      <input
+        type="password"
+        id="keypadInput"
+        value={inputValue}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
 
 export default Keypad;
